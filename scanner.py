f49a23e56b49aa93e27885648478cc2a917020d0
Timmy-Beal <tbeal@highpoint.edu>
2020-11-19 14:52:09 -0500

    updated scanner.py

--- scanner.py
+++ scanner.py
@@ -7,7 +7,12 @@ __maintainer__ = "Timothy Beal"
 __status__ = "In Development"
 
 
-from socket import *
+from scapy.all import *
+
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+import random
 
 import time
 
@@ -22,12 +27,30 @@ class Scanner:
     Instantiates Targets() and Detector()
     """
     def scan(ip):
-        for i in range(1, 1024):
-            s = socket(AF_INET, SOCK_STREAM)
-            if i == 25:
-                i += 1
-            connection = s.connect_ex((ip, i))
-            if connection == 0:
-                print("Port ", i,": OPEN", sep='')
-            s.close()
+        port_rng=[22,23,80,443]
+        for dst_p in port_rng:
+            src_p = random.randint(1024,65534)
+            resp = sr1(
+                IP (dst=ip) / TCP (sport = src_p, dport = dst_p,
+                flags = "S"), timeout=1, verbose = 0)
+            if resp is None:
+                print(f"{ip}:{dst_p} is filtered.")
+
+            elif resp.haslayer(TCP):
+                if resp.getlayer(TCP).flags == 0x12:
+                    send_rst = sr(
+                        IP (dst=ip) / TCP (sport = src_p, dport = dst_p,
+                        flags='R'), timeout = 1, verbose = 0)
+                    print(f"{ip}:{dst_p} is open.")
+            elif resp.getlayer(TCP).flags == 0x14:
+                print(f"{ip}:{dst_p} is closed.")
+
+            elif resp.haslayer(ICMP):
+                if(
+                    int(resp.getlayer(ICMP).type) == 3 and
+                    int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]
+                ):
+                    print(f"{ip}:{dst_p} is filtered.")
+
+
         print("Time Taken:", time.time() - startTime)
