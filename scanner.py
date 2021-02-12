081abef4825fe90efd4cc048a6f3644822c1b14d
Timmy-Beal <tbeal@highpoint.edu>
2021-02-12 16:36:43 -0500

    uses subprocess of nmap

--- scanner.py
+++ scanner.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 __author__ = "Timothy Beal"
 __copyright__ = "Copyright 2020"
 __credits__ = ["Timothy Beal"]
@@ -6,17 +8,9 @@ __version__ = "0.0.1"
 __maintainer__ = "Timothy Beal"
 __status__ = "In Development"
 
+import subprocess
 
-from scapy.all import *
-
-import logging
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-
-import random
-
-import time
-
-startTime = time.time()
+import os
 
 class Scanner:
     """
@@ -27,30 +21,19 @@ class Scanner:
     Instantiates Targets() and Detector()
     """
     def scan(ip):
-        port_rng=[22,23,80,443]
-        for dst_p in port_rng:
-            src_p = random.randint(1024,65534)
-            resp = sr1(
-                IP (dst=ip) / TCP (sport = src_p, dport = dst_p,
-                flags = "S"), timeout=1, verbose = 0)
-            if resp is None:
-                print(f"{ip}:{dst_p} is filtered.")
+        test = False
+        output = subprocess.check_output(["sudo", "nmap", "-O", "-oN", "temp.txt", ip])
+        inFile = open('temp.txt', 'r')
 
-            elif resp.haslayer(TCP):
-                if resp.getlayer(TCP).flags == 0x12:
-                    send_rst = sr(
-                        IP (dst=ip) / TCP (sport = src_p, dport = dst_p,
-                        flags='R'), timeout = 1, verbose = 0)
-                    print(f"{ip}:{dst_p} is open.")
-            elif resp.getlayer(TCP).flags == 0x14:
-                print(f"{ip}:{dst_p} is closed.")
-
-            elif resp.haslayer(ICMP):
-                if(
-                    int(resp.getlayer(ICMP).type) == 3 and
-                    int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]
-                ):
-                    print(f"{ip}:{dst_p} is filtered.")
-
-
-        print("Time Taken:", time.time() - startTime)
+        while test is False:
+            line = inFile.readline()
+            if line[0] == 'R':
+                test=True
+                print(line)
+           
+        test = line.split(': ')
+        print(test[1])
+        test2 = test[1].split(', ')
+        print(test2[0])
+        print(test2[1])
+        print(test2[2])
