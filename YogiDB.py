2cae3084629e1aa3cf7939cda4361f1b57ae2a78
charamis0226 <charamis@highpoint.edu>
2021-03-30 12:58:31 -0400

    Query update
    Added connection object. This allows the database to have queries.

--- YogiDB.py
+++ YogiDB.py
@@ -18,11 +18,30 @@ table2 = """CREATE TABLE IF NOT EXISTS
 Fingerprint(Fingerprint_ID INTEGER PRIMARY KEY, OS STRING, Packets_S FILE, IP_addr STRING, Firewwall_Status STRING,
 Packets_R File)"""
 
+# Creates honeypot_characteristics table. This is a linking table making a one to many relationship with Fingerprint and Por.
+# The primary keys for Port and Fingerprint will be used as both foreign keys and as a composite primary key.
+
+table3 = """CREATE TABLE IF NOT EXISTS
+Honeypot_Characteristics(Fingerprint_ID INTEGER PRIMARY KEY FOREIGN KEY, Port_ID INTEGER PRIMARY KEY FOREIGN KEY)
+"""
+
+def create_connection(Yogi_Database.db):
+    """ create databse connection to the SQLite databse
+    specified by the db_file
+    : param db_file: databse file
+    : return: Connection object or None
+    """
+
+    conn = None
+
+    try:
+        conn = sqlite3.connect(Yogi_Database.db)
+
+    except Error as e:
+        print(e)
 
 
-    
-
-
+    return conn
 
 
 
